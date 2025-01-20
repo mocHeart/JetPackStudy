944ffef9a1c40206ddec9df07f6ee656ad17f7ba
yzddo <739891769@qq.com>
2025-01-20 19:45:25 +0800

    Chronometer计时器组件的基本使用

--- app/src/main/java/com/hg/jps/demo01/MainActivity.kt
+++ app/src/main/java/com/hg/jps/demo01/MainActivity.kt
@@ -1,14 +1,34 @@
 package com.hg.jps.demo01
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.SystemClock
+import android.widget.Chronometer
 import androidx.appcompat.app.AppCompatActivity
 import com.hg.jps.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var chronometer: Chronometer
+    private var elapseTime: Long = 0
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        chronometer = findViewById(R.id.chronometer)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        chronometer.base = SystemClock.elapsedRealtime() - elapseTime
+        chronometer.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        elapseTime = SystemClock.elapsedRealtime() - chronometer.base
+        chronometer.stop()
     }
 }
